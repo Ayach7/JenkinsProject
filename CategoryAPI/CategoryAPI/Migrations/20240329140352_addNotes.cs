55839dfccabe318458122e25ce022e012726d965
Aya Chouitar <chouitaraya3@gmail.com>
2024-12-24 17:32:50 +0100

    Ajout du projet

---
+++ CategoryAPI/CategoryAPI/Migrations/20240329140352_addNotes.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CategoryAPI.Migrations
+{
+    public partial class addNotes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Notes",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Notes",
+                table: "Categories");
+        }
+    }
+}
---
+++ CategoryAPI/CategoryAPI/Model/MyContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CategoryAPI.Model
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Item> Items { get; set; }
+    }
+}
